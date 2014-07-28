1ab530dc538b6eea4edfc819e2102ec9a9540130
Martijn <tijntje_7@msn.com>
2014-07-28 21:26:41 +0200

    Replaced Meteor.userId() with this.userId, since Meteor.userId can only be used in method calls

--- server/publications.js
+++ server/publications.js
@@ -1,12 +1,23 @@
 Meteor.publish("Events", function () {
-    return Events.find({});
+    return Events.find({
+        "_id": this.userId
+    });
 });
 
 Meteor.publish("allUsers", function () {
     return Meteor.users.find({}, {
         fields: {
-           username: 1,
-           friends: 1
+           "username": 1,
+           "profile.friends": 1
         }
     });
 });
+
+// Meteor.publish("friends", function () {
+//     return Meteor.users.find({
+//         "profile.friends._id": Meteor.userId()
+//     }, fields: {
+//         username: 1,
+//         profile: 1
+//     });
+// });
